340a0e2d0a7c22a62adac91767839e4eb1a66649
= <=>
2023-09-12 11:08:32 +0300

    rename var date

--- mietlabs/Exam.cs
+++ mietlabs/Exam.cs
@@ -8,27 +8,27 @@ namespace mietlabs
 {
     internal class Exam
     {
-        public Exam(string name_sub, int mark, System.DateTime date)
+        public Exam(string name_sub, int mark, System.DateTime date_exam)
         {
             this.name_sub = name_sub;
             this.mark = mark;
-            this.date = date;
+            this.date_exam = date_exam;
         }
 
         public Exam()
         {
             name_sub = "";
             mark = 0;
-            date = new DateTime(1978, 1, 1);
+            date_exam = new DateTime(1978, 1, 1);
         }
-        public string name_sub;
-        public int mark;
-        public System.DateTime date;
-
         public override string ToString()
         {
-            return Convert.ToString(name_sub) + " " + Convert.ToString(mark) + " " + Convert.ToString(date);
+            return Convert.ToString(name_sub) + " " + Convert.ToString(mark) + " " + Convert.ToString(date_exam);
         }
+
+        public string name_sub;
+        public int mark;
+        public System.DateTime date_exam;
 
     }
 }
